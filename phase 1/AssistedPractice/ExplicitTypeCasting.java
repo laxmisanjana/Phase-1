6c0b38c9cfa95f58d833745a7c613a0b1d3885bb
laxmisanjana <98545219+laxmisanjana@users.noreply.github.com>
2022-02-10 13:58:39 +0530

    Add files via upload

---
+++ phase 1/AssistedPractice/ExplicitTypeCasting.java
@@ -0,0 +1,11 @@
+public class ExplicitTypeCasting {
+
+	public static void main(String[] args) {
+		
+		double a=78.12;
+		
+		int b=(int) a;
+		
+		System.out.println("Converted double "+a+ " to int "+b);
+	}
+}
